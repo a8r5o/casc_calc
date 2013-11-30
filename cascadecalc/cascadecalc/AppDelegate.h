f94caae8bf717248fd7cd1939ab517166dc7aa61
a8r5o <andreas@rosland.net>
2013-11-30 01:14:37 +0100

    iosversion

---
+++ cascadecalc/cascadecalc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  cascadecalc
+//
+//  Created by Andreas Rosland on 30.11.13.
+//  Copyright (c) 2013 Andreas Rosland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
